42d2e67722d3bfceccaa9ba930a4febb27fe5a78
Jorge Morales <jorge.iv.morales@gmail.com>
2019-08-11 01:35:47 -0500

    Ejercicios basicos de JavaScript

---
+++ ejercicios/02.condicionales.js
@@ -0,0 +1,71 @@
+/**
+ * Condicionales
+ */
+
+// Reemplaza false con la condicion que debes crear en cada ejercicio.
+
+/*
+ Mayor que: >
+ Escribe una condición que verifique que la edad es mayor de 65
+*/
+
+var edadMayor = 88;
+var respuestaMayor;
+
+if (false) {
+  respuestaMayor = 'Ya debe estar jubilado';
+}
+
+/*
+ Menor que: <
+ Escribe una condición que verifique que la edad es menor que 0
+*/
+
+var edadMenor = -20;
+var respuestaMenor;
+
+if (false) {
+  respuestaMenor = 'No ha nacido';
+}
+
+/*
+ Igualdad: ===
+ Escribe una condición que verifique que la edad es igual a 18
+*/
+
+var edadCedula = 18;
+var respuestaCedula;
+
+if (false) {
+  respuestaCedula = 'Ya tiene cedula';
+}
+
+/*
+  Composición de condicionales
+*/
+
+/*
+  OR: ||
+  Escribe una condicion que verifique Si esta cansado o tiene hambre
+*/
+
+var estaCansado = true;
+var tieneHambre = true;
+var respuestaBostezo;
+
+if (false) {
+  respuestaBostezo = 'Va a bostezar';
+}
+
+/*
+ AND: &&
+ Escribe una condicion que verifique si se acosto tarde y se le olvido poner la alarma
+*/
+
+var seQuedoDormido = true;
+var seLeOlvidoLaAlarma = true;
+var respuestaDormido;
+
+if (false) {
+  respuestaDormido = 'Se quedo dormido';
+}
